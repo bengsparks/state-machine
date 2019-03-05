5b3dfd1e0c5ded725ac3130818fb4b5c7087aa5c
Benjamin Sparks <bsparks100@gmail.com>
2019-03-05 20:18:20 +0100

    clang-format

--- main.cpp
+++ main.cpp
@@ -4,31 +4,31 @@
 
 #include "state_machine.hpp"
 
-auto main(int argc, char* argv[]) -> int {
-    auto transitions = sm_ns::transitions{
-        sm_ns::make_transition("stopped", "running", "run", []() -> void {
-                std::cout << "stopped -> running : run\n";
-        }),
-        sm_ns::make_transition("paused", "running", "run", []() -> void {
-                std::cout << "paused -> running : run\n";
-        }),
+auto main(int argc, char* argv[]) -> int
+{
+	auto transitions = sm_ns::transitions{
+		sm_ns::make_transition(
+			"stopped", "running", "run",
+			[]() -> void { std::cout << "stopped -> running : run\n"; }),
+		sm_ns::make_transition(
+			"paused", "running", "run",
+			[]() -> void { std::cout << "paused -> running : run\n"; }),
 
-        sm_ns::make_transition("running", "stopped", "stop", []() -> void {
-                std::cout << "running -> stopped : stop\n";
-        }),
-        sm_ns::make_transition("paused", "stopped", "stop", []() -> void {
-                std::cout << "paused -> stopped : stop\n";
-        }),
-        sm_ns::make_transition("running", "paused", "pause", []() -> void {
-                std::cout << "running -> paused : pause\n";
-        })
-    };
-    sm_ns::state_machine state_machine{transitions, "stopped"};
+		sm_ns::make_transition(
+			"running", "stopped", "stop",
+			[]() -> void { std::cout << "running -> stopped : stop\n"; }),
+		sm_ns::make_transition(
+			"paused", "stopped", "stop",
+			[]() -> void { std::cout << "paused -> stopped : stop\n"; }),
+		sm_ns::make_transition("running", "paused", "pause", []() -> void {
+			std::cout << "running -> paused : pause\n";
+		})};
+	sm_ns::state_machine state_machine{transitions, "stopped"};
 
-    state_machine.trigger(sm_ns::event{"run"});
-    state_machine.trigger(sm_ns::event{"stop"});
-    state_machine.trigger(sm_ns::event{"run"});
-    state_machine.trigger(sm_ns::event{"pause"});
+	state_machine.trigger(sm_ns::event{"run"});
+	state_machine.trigger(sm_ns::event{"stop"});
+	state_machine.trigger(sm_ns::event{"run"});
+	state_machine.trigger(sm_ns::event{"pause"});
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
